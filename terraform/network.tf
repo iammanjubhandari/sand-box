0e49eb277fd0bb6a5b30ee9e2594a400490c35f8
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-08-25 18:11:41 +0530

    changes vpc

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "iammanjubhandari-vpc-sandbox" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.50.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -8,3 +8,9 @@ resource "aws_vpc" "iammanjubhandari-vpc-sandbox" {
     Env  = "dev"
   }
 }
+
+
+
+
+
+
