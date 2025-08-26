9aa16ca55fbc6264244896692b68edaaa5f1ca53
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-08-26 14:58:28 +0530

    Security group added

--- terraform/network.tf
+++ terraform/network.tf
@@ -48,3 +48,33 @@ resource "aws_route_table_association" "iammanjubhandari-sandbox-public-route-ta
   route_table_id = aws_route_table.iammanjubhandari-sandbox-public-route-table.id
   subnet_id      = aws_subnet.public_subnet.id
 }
+
+resource "aws_security_group" "iammanjubhandari-sandbox-sg" {
+  name        = "dev-vpc-default-sg"
+  description = "Dev VPC Default Security Group"
+  vpc_id      = aws_vpc.iammanjubhandari-vpc-sandbox.id
+
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all IP and Ports Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
