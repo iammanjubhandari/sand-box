f352f580da169399c0ec99009a1ca66af162fd29
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-08-26 15:06:22 +0530

    compute - ec2 instance

---
+++ terraform/compute.tf
@@ -0,0 +1,22 @@
+
+resource "aws_instance" "iammanjubhandari-ec2instance" {
+  ami               = "ami-0df609f69029c9bdb"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-southeast-2a" # Sydney
+  associate_public_ip_address = true
+  vpc_security_group_ids      = aws_security_group.iammanjubhandari-sandbox-sg.id
+  key_name                    = aws_key_pair.iammanjubhandari-key.sandbox
+    user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get upgrade -y
+              apt-get install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+              echo "<h1>🚀 AWS Infra created using Terraform in ap-southeast-2 (Sydney) Region</h1>" > /var/www/html/index.nginx-debian.html
+              EOF
+  tags = {
+    Name = "iammanjubhandari-ec2instance"
+    Env  = "sandbox"
+  }
+}
